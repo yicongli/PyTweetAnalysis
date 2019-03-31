f34cffb39105c7100b0c860c04f15258afe2f814
Yicong Li <liyicong.china@gmail.com>
2019-03-31 23:49:17 +1100

    Finalise the function of reading big json file

--- twitter.py
+++ twitter.py
@@ -3,6 +3,9 @@ import json
 from itertools import islice
 
 def inputFileName():
+  """
+  get json file name
+  """
   if len(sys.argv) != 2:
     print ("twitter.py <twitter_file.json>" )
     # what will return here?
@@ -10,17 +13,41 @@ def inputFileName():
     fileName = sys.argv[1]
     return fileName
 
-def parseJsonData(fileName, range):
+def getFileLinePoint(fileName):
   """
-  The function to parse the Json data in the file 
+  get the line start point, only run in testing
   """
-  #fileName = '/Users/yicongli/Downloads/twitter-melb.json'
-  with open(fileName,'r') as file:
-    for i, line in islice(file, range[0], range[1]):
+  dict_pos = []
+  length = 0
+
+  with open(fileName,'rb') as file:
+    for each in file:
+        dict_pos.append(length)
+        length += len(each)
+
+  with open('conf.json', 'w') as outfile:
+    json.dump(dict_pos, outfile)
+  return dict_pos
+
+def parseJsonDataWithConf(fileName, startLineNum, size):
+  """
+  parse json data according to current arranged period
+  """
+  with open(fileName, 'r') as file:
+    file.seek(startLineNum) # jump to the start point
+    for i, line in enumerate(file):
+      # if larger than current size, then return directly
+      if(i >= size): 
+        return
+
       try:
           data = json.loads(line[:-2]) 
           extractInfoFromData(data)
       except json.decoder.JSONDecodeError:
+        try:
+          data = json.loads(line[:-1]) 
+          extractInfoFromData(data)
+        except json.decoder.JSONDecodeError:
           print('Error on line', i + 1, ':\n', repr(line))
 
 def extractInfoFromData(data):
@@ -50,7 +77,7 @@ def getGeoLocation():
 
 
 from mpi4py import MPI
-CONST_SIZE = 2500000  # the total number of data is 2500000
+CONST_SIZE = 2500000  # the total line number of data is 2500000
 
 if __name__ == "__main__":
   twitterFile = inputFileName()
@@ -60,7 +87,12 @@ if __name__ == "__main__":
   #print(num_lines)
   # for grid in grids:
   #   print (grids[grid])
+
+  with open('conf.json') as json_data:
+    listConf = json.load(json_data)
   
-  comm = MPI.COMM_WORLD
-  iterRange = CONST_SIZE / comm.size
+  #comm = MPI.COMM_WORLD
+  #iterRange = CONST_SIZE / comm.size
+  # Test code
+  parseJsonDataWithConf('/Users/yicongli/Downloads/twitter-melb.json',listConf[0],CONST_SIZE / 4)
 
