c99f86871900b16b745b7e466eccd968cec810ff
Yicong Li <liyicong.china@gmail.com>
2019-03-31 20:48:38 +1100

    Add parse json data function

--- twitter.py
+++ twitter.py
@@ -1,12 +1,33 @@
-import json
 import sys
+import json
+from itertools import islice
 
-def input():
+def inputFileName():
   if len(sys.argv) != 2:
     print ("twitter.py <twitter_file.json>" )
+    # what will return here?
   else:
     fileName = sys.argv[1]
     return fileName
+
+def parseJsonData(fileName, range):
+  """
+  The function to parse the Json data in the file 
+  """
+  #fileName = '/Users/yicongli/Downloads/twitter-melb.json'
+  with open(fileName,'r') as file:
+    for i, line in islice(file, range[0], range[1]):
+      try:
+          data = json.loads(line[:-2]) 
+          extractInfoFromData(data)
+      except json.decoder.JSONDecodeError:
+          print('Error on line', i + 1, ':\n', repr(line))
+
+def extractInfoFromData(data):
+  """
+  This function is to extract the infomation from
+  """
+  pass
 
 def getGeoLocation():
   """
@@ -28,10 +49,18 @@ def getGeoLocation():
   return grids
 
 
+from mpi4py import MPI
+CONST_SIZE = 2500000  # the total number of data is 2500000
+
 if __name__ == "__main__":
-  twitterFile = input()
-  
-  # grids = getGeoLocation()   
+  twitterFile = inputFileName()
+  grids = getGeoLocation()   
+
+  #num_lines = sum(1 for line in open('/Users/yicongli/Downloads/twitter-melb.json'))
+  #print(num_lines)
   # for grid in grids:
   #   print (grids[grid])
+  
+  comm = MPI.COMM_WORLD
+  iterRange = CONST_SIZE / comm.size
 
