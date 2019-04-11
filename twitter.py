f1ce563056b076552c488719d9e671fe05d086cb
thaole <thao.t.le25@gmail.com>
2019-04-12 00:21:15 +1000

    new branch

--- twitter.py
+++ twitter.py
@@ -2,54 +2,52 @@ import sys
 import json
 from mpi4py import MPI
 from operator import itemgetter
+import os.path
 
-class CaseInsensitiveDict(dict):
+# class CaseInsensitiveDict(dict):
+#   """
+#   the dict which is case-insensitive
+#   """
+#   def __setitem__(self, key, value):
+#       super(CaseInsensitiveDict, self).__setitem__(key.lower(), value)
+
+#   def __getitem__(self, key):
+#       return super(CaseInsensitiveDict, self).__getitem__(key.lower())
+
+MELBOURNE_GRID_FILE = "melbGrid.json"
+
+def getTwitterFile():
   """
-  the dict which is case-insensitive
+  get twitter file name from command argument
   """
-  def __setitem__(self, key, value):
-      super(CaseInsensitiveDict, self).__setitem__(key.lower(), value)
+  if len(sys.argv) != 2:
+    print ("twitter.py <twitter_file.json>" )
+  else:
+    fileName = sys.argv[1]
+    return fileName
 
-  def __getitem__(self, key):
-      return super(CaseInsensitiveDict, self).__getitem__(key.lower())
-
-def getGeoLocation():
+def getMelbourneGrids():
   """
   Get the range of x and y for each grid box
   """
-  melbGridFile = open('melbGrid.json', 'r')
+  melbGridFile = open(MELBOURNE_GRID_FILE, 'r')
   data = melbGridFile.read()
   melbGrid = json.loads(data)
 
   grids = {}
-  # grids is a dictionary in which:
-  # - key is the id of grid box: A1, A2, ...
-  # - value is the x-range and y-range
-
+  """
+  grids is a dictionary in which:
+    - key is the id of grid box: A1, A2, ...
+    - value is a dictionary contains:
+      + coordinates
+      + numPosts
+      + hashtags: {hashtag: frequency}
+  """
   for feature in melbGrid["features"]:
     coordinates = {"xmin": feature["properties"]["xmin"], "xmax": feature["properties"]["xmax"], "ymin": feature["properties"]["ymin"], "ymax": feature["properties"]["ymax"]}
-    grids[feature["properties"]["id"]] = coordinates
+    grids[feature["properties"]["id"]] = {"coordinates": coordinates, "numPosts": 0, "hashtags": {}}
   
-  return grids
-
-# global variable for storing geolocation grids
-g_grids = getGeoLocation() 
-
-def getFileLinePoint(fileName):
-  """
-  get the line start point, only run in testing
-  """
-  dict_pos = []
-  length = 0
-
-  with open(fileName,'rb') as file:
-    for each in file:
-        dict_pos.append(length)
-        length += len(each)
-  # store the startpoint of each line into file
-  with open('conf.json', 'w') as outfile:
-    json.dump(dict_pos, outfile)
-  return dict_pos
+  return grids 
 
 def initResultDic():
   """
@@ -57,39 +55,14 @@ def initResultDic():
   This result dic is used to store all the info that collected from json data
   """
   resultDic = {}
-  for key, value in g_grids.items():
+  melbGrids = getGeoLocation()
+  for key in melbGrids:
     resultDic[key] = {'posN':0, 'hashtags':CaseInsensitiveDict()}
-  
   return resultDic
-
-def parseJsonDataWithConf(fileName, startLinePoint, size):
-  """
-  parse json data according to current arranged period
-  """
-  result = initResultDic()
-
-  with open(fileName, 'r') as file:
-    file.seek(startLinePoint) # jump to the start point
-    for i, line in enumerate(file):
-      # if larger than current size, then return directly
-      if(i >= size): 
-        return result
-      # extract key info from data
-      try:
-          data = json.loads(line[:-2]) # TODO: May use regular formula to extract data
-          extractInfoFromData(data, result)
-      except json.decoder.JSONDecodeError:
-        try:
-          data = json.loads(line[:-1]) 
-          extractInfoFromData(data, result)
-        except json.decoder.JSONDecodeError:
-          print('Error on line', i + 1, ':\n', repr(line))
-  
-  return result
 
 def extractInfoFromData(data, resultDic):
   """
-  This function is to extract the infomation from
+  This function is used to extract the infomation from
   """
   try:
     dataCoord = data['doc']['coordinates']['coordinates']
@@ -100,7 +73,8 @@ def extractInfoFromData(data, resultDic):
     print('No coordinate')
     return
 
-  for key, value in g_grids.items():
+  melbGrids = getGeoLocation()
+  for key, value in melbGrids.items():
     # if the value in the specific area, then store the data into the result dic
     if (dataCoord[0] <= value['xmax'] and dataCoord[0] > value['xmin'] 
     and dataCoord[1] < value['ymax'] and dataCoord[1] >= value['ymin']):
@@ -118,20 +92,35 @@ def extractInfoFromData(data, resultDic):
           finally:
             resultDic[key]['hashtags'][hashtag['text']] = hashtagNum + 1
 
-def inputFileName():
+def parseJsonDataWithConf(fileName, startLinePoint, size):
   """
-  get json file name
+  parse json data according to current arranged period
   """
-  if len(sys.argv) != 2:
-    print ("twitter.py <twitter_file.json>" )
-  else:
-    fileName = sys.argv[1]
-    return fileName
+  result = initResultDic()
 
+  with open(fileName, 'r') as file:
+    file.seek(startLinePoint) # jump to the start point
+    for i, line in enumerate(file):
+      # if larger than current size, then return directly
+      if(i >= size): 
+        return result
+      # extract key info from data
+      try:
+          data = json.loads(line[:-2])
+          extractInfoFromData(data, result)
+      except json.decoder.JSONDecodeError:
+        # NOTE: In what case we need to extract line[:-1]?
+        try:
+          data = json.loads(line[:-1]) 
+          extractInfoFromData(data, result)
+        except json.decoder.JSONDecodeError:
+          print('Error on line', i + 1, ':\n', repr(line))
+  
+  return result
 
 def removeRedundantHashtags(resultDic):
   """
-  remove the hashtags that has really small number in the result,
+  remove the hashtags that have really small number in the result,
   only reserve the largest ten tags
   """
   for key, value in resultDic.items():
@@ -182,7 +171,6 @@ def orderTheResultIntoList(resultDic):
   resultList = [(k, resultDic[k]) for k in sorted(resultDic, key=lambda x: resultDic[x]['posN'], reverse=True)]
   return resultList
 
-
 def prettyPrint(resultList):
   #print pretty
   print('The rank of Grid boxes:')
@@ -197,20 +185,21 @@ def prettyPrint(resultList):
       print('(#%s, %d), ' % (hashtag[0], hashtag[1]), end='')
     print(')')
 
-CONST_SIZE = 2500000  # the total line number of data is 2500000
-
 if __name__ == "__main__":
-  #load configuration of big json data
-  with open('conf.json') as json_data:
-    listConf = json.load(json_data)
-  
-  comm = MPI.COMM_WORLD
-  iterRange = int(CONST_SIZE / comm.size) # get the range of iteration
-  startLineNum = comm.rank * iterRange + 1
+  twitterFile = getTwitterFile()
 
-  twitterFile = inputFileName()
-  result = parseJsonDataWithConf(twitterFile,listConf[startLineNum], iterRange)
-  removeRedundantHashtags(result) # may need to remove the hashtag that have really small number
+  comm = MPI.COMM_WORLD
+  size = comm.size
+  rank = comm.rank
+
+  # Master 
+  if rank == 0:
+
+
+  # numLinesEachProcessor = int(CONST_SIZE / numProcessors) # get the range of iteration
+  # startLineNum = comm.rank * numLinesEachProcessor + 1
+  # result = parseJsonDataWithConf(twitterFile, listConf[startLineNum], numLinesEachProcessor)
+  # removeRedundantHashtags(result) # may need to remove the hashtag that have really small number
 
   # gather data all together
   data = comm.gather(result)
